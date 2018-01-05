98bc7d2662ca0852545da044606b0e916063c3b5
Abner_G <grh_1990@126.com>
2018-01-05 16:51:18 +0800

    update

--- RHKit/RHKit.h
+++ RHKit/RHKit.h
@@ -12,31 +12,17 @@
 
 #import <RHKit/NSData+RHAdd.h>
 #import <RHKit/NSString+RHAdd.h>
-#import <RHKit/NSArray+RHAdd.h>
-#import <RHKit/NSDictionary+RHAdd.h>
 #import <RHKit/NSObject+RHAdd.h>
-//#import <RHKit/+RHAdd.h>
 
 #import <RHKit/UIColor+RHAdd.h>
 #import <RHKit/UIView+RHAdd.h>
 #import <RHKit/UIImage+RHAdd.h>
-#import <RHKit/UIImageView+RHAdd.h>
-#import <RHKit/UILabel+RHAdd.h>
-#import <RHKit/UIButton+RHAdd.h>
-#import <RHKit/UITextField+RHAdd.h>
-#import <RHKit/UITextView+RHAdd.h>
-#import <RHKit/UITableView+RHAdd.h>
-//#import <RHKit/+RHAdd.h>
-//#import <RHKit/+RHAdd.h>
+#import <RHKit/UIPickerView+RHAdd.h>
 
 #import <RHKit/RHView.h>
 #import <RHKit/RHLabel.h>
 #import <RHKit/RHButton.h>
 #import <RHKit/RHImageView.h>
-#import <RHKit/RHTextField.h>
-#import <RHKit/RHTextView.h>
-//#import <RHKit/>
-//#import <RHKit/>
 
 #import <RHKit/RHApplication.h>
 #import <RHKit/RHPath.h>
@@ -46,26 +32,17 @@
 
 #import "NSData+RHAdd.h"
 #import "NSString+RHAdd.h"
-#import "NSArray+RHAdd.h"
-#import "NSDictionary+RHAdd.h"
 #import "NSObject+RHAdd.h"
 
 #import "UIColor+RHAdd.h"
 #import "UIView+RHAdd.h"
 #import "UIImage+RHAdd.h"
-#import "UIImageView+RHAdd.h"
-#import "UILabel+RHAdd.h"
-#import "UIButton+RHAdd.h"
-#import "UITextField+RHAdd.h"
-#import "UITextView+RHAdd.h"
-#import "UITableView+RHAdd.h"
+#import "UIPickerView+RHAdd.h"
 
 #import "RHView.h"
 #import "RHLabel.h"
 #import "RHButton.h"
 #import "RHImageView.h"
-#import "RHTextField.h"
-#import "RHTextView.h"
 
 #import "RHApplication.h"
 #import "RHPath.h"
