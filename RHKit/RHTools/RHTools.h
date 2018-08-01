7d66c800d301757d339abc2d14428a273ba0a0cb
Abner_G <grh_1990@126.com>
2018-08-01 11:36:10 +0800

    update

--- RHKit/RHTools/RHTools.h
+++ RHKit/RHTools/RHTools.h
@@ -72,23 +72,102 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark - 文字宽高
 
 /**
- 获取高度
+ 获取文字高度
  
+ @param text  文字
+ @param font  字体
  @param width 宽度
- @param text 文字
- @param font 字体
- @return 高度
+ @return      文字高度
  */
 + (CGFloat)getHeightByText:(NSString *)text font:(UIFont *)font width:(CGFloat)width;
 
 /**
- 获取宽度
+ 获取文字高度
+ 
+ @param text        文字
+ @param font        字体
+ @param lineSpacing 行间距
+ @param width       宽度
+ @return            文字高度
+ */
++ (CGFloat)getHeightByText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing width:(CGFloat)width;
+
+/**
+ 获取文字高度
+ 
+ @param attributedText 属性文字
+ @param width          宽度
+ @return               文字高度
+ */
++ (CGFloat)getHeightByAttributedText:(NSAttributedString *)attributedText width:(CGFloat)width;
+
+/**
+ 获取文字宽度
  
  @param text 文字
  @param font 字体
- @return 宽度
+ @return     文字宽度
  */
 + (CGFloat)getWidthByText:(NSString *)text font:(UIFont *)font;
+
+/**
+ 获取文字宽度
+ 
+ @param attributedText 属性文字
+ @return               文字宽度
+ */
++ (CGFloat)getWidthByAttributedText:(NSAttributedString *)attributedText;
+
+#pragma mark - 系统相关
+
+/**
+ 获取设备名称
+ 
+ @return 设备名称  e.g. "My iPhone"
+ */
++ (NSString *)getDeviceName;
+
+/**
+ 获取设备类型
+ 
+ @return 设备类型 e.g. @"iPhone", @"iPod touch"
+ */
++ (NSString *)getDeviceModel;
+
+/**
+ 获取系统名称
+ 
+ @return 系统名称 e.g. @"iOS"
+ */
++ (NSString *)getSystemName;
+
+/**
+ 获取系统版本
+ 
+ @return 系统版本 e.g. @"4.0"
+ */
++ (NSString *)getSystemVersion;
+
+/**
+ 获取系统语言
+ 
+ @return 系统语言 e.g. @"zh-Hans"
+ */
++ (NSString *)getSystemLanguage;
+
+/**
+ 获取当前所在地
+ 
+ @return 当前所在地
+ */
++ (NSString *)getLocaleName;
+
+/**
+ 获取当前所在地使用语言
+ 
+ @return 当前所在地使用语言
+ */
++ (NSString *)getLocaleLanguage;
 
 #pragma mark - 权限相关
 
--- RHKit/RHTools/RHTools.m
+++ RHKit/RHTools/RHTools.m
@@ -123,15 +123,19 @@
 #pragma mark - 文字宽高
 
 /**
- 获取高度
+ 获取文字高度
  
+ @param text  文字
+ @param font  字体
  @param width 宽度
- @param text 文字
- @param font 字体
- @return 高度
+ @return      文字高度
  */
 + (CGFloat)getHeightByText:(NSString *)text font:(UIFont *)font width:(CGFloat)width {
     
+    if (text.length == 0 || width <= 0) {
+        
+        return 0;
+    }
     UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, 0)];
     label.text = text;
     label.font = font;
@@ -142,14 +146,76 @@
 }
 
 /**
- 获取宽度
+ 获取文字高度
+ 
+ @param text        文字
+ @param font        字体
+ @param lineSpacing 行间距
+ @param width       宽度
+ @return            文字高度
+ */
++ (CGFloat)getHeightByText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing width:(CGFloat)width {
+    
+    if (text.length == 0 || width <= 0) {
+        
+        return 0;
+    }
+    UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, 0)];
+    if (lineSpacing > 0) {
+        
+        NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+        paragraphStyle.lineSpacing = lineSpacing;
+        NSDictionary * attributes = @{
+                                      NSFontAttributeName : font,
+                                      NSParagraphStyleAttributeName : paragraphStyle
+                                      };
+        NSAttributedString * attStr = [[NSAttributedString alloc] initWithString:text attributes:attributes];
+        label.attributedText = attStr;
+    } else {
+        
+        label.text = text;
+        label.font = font;
+    }
+    label.numberOfLines = 0;
+    [label sizeToFit];
+    CGFloat height = label.frame.size.height;
+    return height;
+}
+
+/**
+ 获取文字高度
+ 
+ @param attributedText 属性文字
+ @param width          宽度
+ @return               文字高度
+ */
++ (CGFloat)getHeightByAttributedText:(NSAttributedString *)attributedText width:(CGFloat)width {
+    
+    if (attributedText.length == 0 || width <= 0) {
+        
+        return 0;
+    }
+    UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, 0)];
+    label.attributedText = attributedText;
+    label.numberOfLines = 0;
+    [label sizeToFit];
+    CGFloat height = label.frame.size.height;
+    return height;
+}
+
+/**
+ 获取文字宽度
  
  @param text 文字
  @param font 字体
- @return 宽度
+ @return     文字宽度
  */
 + (CGFloat)getWidthByText:(NSString *)text font:(UIFont *)font {
     
+    if (text.length == 0) {
+        
+        return 0;
+    }
     UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, MAXFLOAT, 0)];
     label.text = text;
     label.font = font;
@@ -157,6 +223,103 @@
     CGFloat width = label.frame.size.width;
     return width;
 }
+
+/**
+ 获取文字宽度
+ 
+ @param attributedText 属性文字
+ @return               文字宽度
+ */
++ (CGFloat)getWidthByAttributedText:(NSAttributedString *)attributedText {
+    
+    if (attributedText.length == 0) {
+        
+        return 0;
+    }
+    UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, MAXFLOAT, 0)];
+    label.attributedText = attributedText;
+    [label sizeToFit];
+    CGFloat width = label.frame.size.width;
+    return width;
+}
+
+#pragma mark - 系统相关
+
+/**
+ 获取设备名称
+ 
+ @return 设备名称  e.g. "My iPhone"
+ */
++ (NSString *)getDeviceName {
+    
+    return [[UIDevice currentDevice] name];
+}
+
+/**
+ 获取设备类型
+ 
+ @return 设备类型 e.g. @"iPhone", @"iPod touch"
+ */
++ (NSString *)getDeviceModel {
+    
+    return [[UIDevice currentDevice] model];
+}
+
+/**
+ 获取系统名称
+ 
+ @return 系统名称 e.g. @"iOS"
+ */
++ (NSString *)getSystemName {
+    
+    return [[UIDevice currentDevice] systemName];
+}
+
+/**
+ 获取系统版本
+ 
+ @return 系统版本 e.g. @"4.0"
+ */
++ (NSString *)getSystemVersion {
+    
+    return [[UIDevice currentDevice] systemVersion];
+}
+
+/**
+ 获取系统语言
+ 
+ @return 系统语言 e.g. @"zh-Hans"
+ */
++ (NSString *)getSystemLanguage {
+    
+    NSUserDefaults * defaults = [NSUserDefaults standardUserDefaults];
+    NSArray * allLanguages = [defaults objectForKey:@"AppleLanguages"];
+    // zh-Hans是简体中文，zh-Hant是繁体中文。。。
+    return allLanguages.firstObject;
+}
+
+/**
+ 获取当前所在地
+ 
+ @return 当前所在地
+ */
++ (NSString *)getLocaleName {
+    
+    NSString * identifier = [[NSLocale currentLocale] localeIdentifier];
+    NSString * displayName = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:identifier];
+    return displayName;
+}
+
+/**
+ 获取当前所在地使用语言
+ 
+ @return 当前所在地使用语言
+ */
++ (NSString *)getLocaleLanguage {
+    
+    return [[NSLocale currentLocale] objectForKey:NSLocaleLanguageCode];
+}
+
 
 #pragma mark - 权限相关
 
